afcbf8e09db3274d4d6a20c669e4e2d021360054
michael-masarik <masarikfamilymichael@gmail.com>
2026-05-15 16:41:27 -0500

    Added plumfieldpress and plumfieldmoms

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,3 +1,4 @@
+import pressLogo from "@/assets/press-logo.png";
 import { Badge } from "@/components/ui/badge";
 import { Button } from "./ui/button";
 import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } from "./ui/card";
@@ -5,11 +6,11 @@ import { Card, CardAction, CardDescription, CardFooter, CardHeader, CardTitle } 
 export const Gallery = () => (
     <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-4">
         <Card className="w-full">
-            <img className="relative z-20 aspect-video w-full object-cover" src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/2498cd74-8281-4216-9998-1819f68cea57/Plumfield_Library_for_Web/w=1920,quality=90,fit=scale-down" />
+            <img className="relative z-20 aspect-video w-full object-cover h-45" src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/2498cd74-8281-4216-9998-1819f68cea57/Plumfield_Library_for_Web/w=1920,quality=90,fit=scale-down" />
             <CardHeader>
 
                 <CardAction>
-                    <Badge>Library</Badge>
+                    <Badge variant="secondary">Library</Badge>
                 </CardAction>
                 <CardTitle>
                     Plumfield Library
@@ -23,11 +24,11 @@ export const Gallery = () => (
             </CardFooter>
         </Card>
         <Card className="w-full">
-            <img className="relative z-20 aspect-video w-full object-cover" src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/e5f98bc9-5db8-494c-bce6-abcc7fb520ad/2/w=1920,quality=90,fit=scale-down" />
+            <img className="relative z-20 aspect-video w-full object-cover h-45" src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/e5f98bc9-5db8-494c-bce6-abcc7fb520ad/2/w=1920,quality=90,fit=scale-down" />
             <CardHeader>
 
                 <CardAction>
-                    <Badge>Library</Badge>
+                    <Badge variant="secondary">Library</Badge>
                 </CardAction>
                 <CardTitle>
                     Split Rocks Lending Library
@@ -38,6 +39,51 @@ export const Gallery = () => (
             </CardHeader>
             <CardFooter className="mt-auto">
                 <Button className="w-full" asChild><a href="https://splitrockslibrary.com" target="_blank">View Website</a></Button>
+            </CardFooter>
+        </Card>
+        <Card className="w-full">
+            <div className="relative z-20 aspect-video w-full object-cover h-45  bg-plum mt-0 pt-0">
+                <div className="flex h-full w-full items-center justify-center">
+                    <img
+                        className="max-h-3/4 max-w-3/4 object-contain"
+                        src={pressLogo}
+                        alt="Centered"
+                    />
+                </div>
+            </div>
+            <CardHeader>
+
+                <CardAction>
+                    <Badge variant="secondary">Press</Badge>
+                </CardAction>
+                <CardTitle>
+                    Plumfield Press
+                </CardTitle>
+                <CardDescription>
+                    A Small Press Built by Librarians for Librarians, Families, and Schools
+                </CardDescription>
+            </CardHeader>
+            <CardFooter className="mt-auto">
+                <Button className="w-full" asChild><a href="https://plumfieldpress.com" target="_blank">View Website</a></Button>
+            </CardFooter>
+        </Card>
+        <Card className="w-full">
+            <img className="relative z-20 aspect-video w-full object-cover h-45" src="https://images.spr.so/cdn-cgi/imagedelivery/j42No7y-dcokJuNgXeA0ig/e6682ba3-a341-4a27-9157-aa44acba5767/cropped-Snap_the_Whip_1872_Winslow_Homer/w=1920,quality=90,fit=scale-down" />
+
+            <CardHeader>
+
+                <CardAction>
+                    <Badge variant="secondary">Blog</Badge>
+                </CardAction>
+                <CardTitle>
+                    Plumfield Mom
+                </CardTitle>
+                <CardDescription>
+                    Christians, Book Reviewers, Podcasters, Lovers of Good, True, and Beautiful Books. Plumfield Moms is for families and living books lending librarians.
+                </CardDescription>
+            </CardHeader>
+            <CardFooter className="mt-auto">
+                <Button className="w-full" asChild><a href="https://plumfieldmoms.com" target="_blank">View Website</a></Button>
             </CardFooter>
         </Card>
     </div>
--- src/components/ui/card.tsx
+++ src/components/ui/card.tsx
@@ -12,7 +12,7 @@ function Card({
       data-slot="card"
       data-size={size}
       className={cn(
-        "group/card flex flex-col gap-6 overflow-hidden rounded-4xl bg-card py-6 text-sm text-card-foreground shadow-md ring-1 ring-foreground/5 has-[>img:first-child]:pt-0 data-[size=sm]:gap-4 data-[size=sm]:py-4 dark:ring-foreground/10 *:[img:first-child]:rounded-t-4xl *:[img:last-child]:rounded-b-4xl",
+        "group/card flex flex-col gap-6 overflow-hidden rounded-4xl bg-card py-6 text-sm text-card-foreground shadow-md ring-1 ring-foreground/5 has-[>img:first-child]:pt-0 has-[>div:first-child]:pt-0 data-[size=sm]:gap-4 data-[size=sm]:py-4 dark:ring-foreground/10 *:[img:first-child]:rounded-t-4xl *:[img:last-child]:rounded-b-4xl",
         className
       )}
       {...props}
@@ -90,11 +90,6 @@ function CardFooter({ className, ...props }: React.ComponentProps<"div">) {
 }
 
 export {
-  Card,
-  CardHeader,
-  CardFooter,
-  CardTitle,
-  CardAction,
-  CardDescription,
-  CardContent,
+  Card, CardAction, CardContent, CardDescription, CardFooter, CardHeader, CardTitle
 }
+
